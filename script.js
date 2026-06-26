3e03d77d88284a11cd68cf65b1014c467fcabc2f
Jetross <jetrossneri07@gmail.com>
2026-06-26 15:16:27 +0800

    feat: add button 7 and clear button

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 lucide.createIcons();
+
+let output = 0;
+
+//action buttons
+const clear = document.getElementById("clear");
+
+//number buttons
+const button7 = document.getElementById("button7");
+
+document.getElementById("output").innerHTML = output;
+
+//Clear Button
+clear.addEventListener("click", () => {
+  output = 0;
+  document.getElementById("output").innerHTML = output;
+});
+
+//Button 7
+button7.addEventListener("click", () => {
+  output = 7;
+  document.getElementById("output").innerHTML = output;
+});
