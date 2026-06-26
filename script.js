d5c2b0fc1ca9619ad0e50a7bf73c74c4600578f7
Jetross <jetrossneri07@gmail.com>
2026-06-26 15:31:06 +0800

    feat: add if for button 4 5 and 6

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const clear = document.getElementById("clear");
 const button7 = document.getElementById("button7");
 const button8 = document.getElementById("button8");
 const button9 = document.getElementById("button9");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+const button6 = document.getElementById("button6");
 
 document.getElementById("output").innerHTML = output;
 
@@ -33,5 +36,23 @@ button8.addEventListener("click", () => {
 //Button 9
 button9.addEventListener("click", () => {
   output = 9;
+  document.getElementById("output").innerHTML = output;
+});
+
+//Button 4
+button4.addEventListener("click", () => {
+  output = 4;
+  document.getElementById("output").innerHTML = output;
+});
+
+//Button5
+button5.addEventListener("click", () => {
+  output = 5;
+  document.getElementById("output").innerHTML = output;
+});
+
+//Button 6
+button6.addEventListener("click", () => {
+  output = 6;
   document.getElementById("output").innerHTML = output;
 });
