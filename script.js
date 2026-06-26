dac74a81edaeaaf310722625ff2467271de52301
Jetross <jetrossneri07@gmail.com>
2026-06-26 16:24:28 +0800

    feat: add if else statement on every number button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 lucide.createIcons();
 
-let output = 0;
+let output = ["0"];
 
 //action buttons
 const clear = document.getElementById("clear");
@@ -20,60 +20,105 @@ document.getElementById("output").innerHTML = output;
 
 //Clear Button
 clear.addEventListener("click", () => {
-  output = 0;
+  output = ["0"];
   document.getElementById("output").innerHTML = output;
 });
 
 //Button 7
 button7.addEventListener("click", () => {
-  output = 7;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["7"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["7"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 8
 button8.addEventListener("click", () => {
-  output = 8;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["8"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["8"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 9
 button9.addEventListener("click", () => {
-  output = 9;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["9"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["9"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 4
 button4.addEventListener("click", () => {
-  output = 4;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["4"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["4"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button5
 button5.addEventListener("click", () => {
-  output = 5;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["5"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["5"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 6
 button6.addEventListener("click", () => {
-  output = 6;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["6"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["6"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 1
 button1.addEventListener("click", () => {
-  output = 1;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["1"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["1"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 2
 button2.addEventListener("click", () => {
-  output = 2;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["2"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["2"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
 
 //Button 3
 button3.addEventListener("click", () => {
-  output = 3;
-  document.getElementById("output").innerHTML = output;
+  if (output.length == 1 && output[0] == "0") {
+    output[0] = ["3"];
+    document.getElementById("output").innerHTML = output;
+  } else {
+    output += ["3"];
+    document.getElementById("output").innerHTML = output;
+  }
 });
